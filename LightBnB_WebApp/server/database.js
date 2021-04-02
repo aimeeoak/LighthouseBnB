9603bda6ceba9a2d6c160cec2625668bd0bab55a
Aimee <aimeegarriock@gmail.com>
2021-04-02 05:11:35 +0000

    fixing stupid mistake with code for getting reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -47,7 +47,6 @@ exports.getUserWithEmail = getUserWithEmail;
       WHERE id = $1`,
       [id]
     )
-
     .then((res) => {
       if (res) {
         console.log("Login successful");
@@ -66,12 +65,16 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+ const addUser = function (user) {
+  return pool
+    .query(
+      `INSERT INTO users(name, email, password)
+      VALUES($1 ,$2, $3)
+      RETURNING *;`,
+      [user.name, user.email, user.password]
+    )
+    .then((res) => res.rows);
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -81,9 +84,22 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+ const getAllReservations = function (id) {
+  return pool
+    .query(
+      `SELECT properties.*, reservations.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      LEFT JOIN property_reviews ON properties.id = property_reviews.property_id 
+      WHERE reservations.guest_id = $1
+      AND reservations.end_date < now()::date
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+     LIMIT 10;`,
+      [id]
+    )
+    .then((res) => res.rows);
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
